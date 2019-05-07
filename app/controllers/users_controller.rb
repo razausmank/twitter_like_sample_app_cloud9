f36eca28ecd6ccf4ad3b2099f9087833965a682d
Usman raza <Razausmankhan97@gmail.com>
2019-05-07 20:29:20 +0000

    add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+    before_action :logged_in_user, only: [:edit, :update, :index, :destroy, :following, :followers]
     before_action :correct_user,   only: [:edit, :update]
     before_action :admin_user,     only: :destroy
     
@@ -30,7 +30,19 @@ class UsersController < ApplicationController
   
  
   
-  
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   
   def admin_user
       redirect_to(root_url) unless current_user.admin?
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,18 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   
   has_many :microposts , dependent: :destroy 
-    
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  
+ has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy  
+                                  
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},format: { with: VALID_EMAIL_REGEX },
@@ -18,8 +29,27 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
   
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
+  
   def feed
-    Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
   
   def User.new_token
