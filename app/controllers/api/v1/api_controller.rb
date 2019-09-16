0de6133e342e9ebfba86da44726526600503d6ba
Usman raza <Razausmankhan97@gmail.com>
2019-09-16 22:23:54 +0000

    testing api

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
-  class ApiController < ApplicationController
-    #API stuff here
+  # app/controllers/api_controller.rb
+  class ApiController < ActionController::API
+  # Do whatever you would do in ApplicationController
   end
 end
+
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,22 @@ module Api::V1
     def index
       respond_with User.first(5)
     end
-
+    
+  def create
+     @user = User.new(user_params)   
+     
+      if @user.save
+        render json:{status: 'Success', message:'created User', data:@user} 
+      else
+        render json:{status: 'Error', message:'created User failed', data:@user.errors} 
+      end
+    end
+  private 
+      
+      def user_params
+          params.require(:user).permit(:name, :email, :password,
+                                         :password_confirmation)
+      end
+  
   end
 end
