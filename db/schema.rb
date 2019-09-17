8e23e3c3b55606a855f1fbaf693e637550271276
Usman raza <Razausmankhan97@gmail.com>
2019-09-17 10:52:17 +0000

    shit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190916231943) do
+ActiveRecord::Schema.define(version: 20190917095351) do
 
   create_table "cars", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20190916231943) do
     t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "pens", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "token"
   end
 
   create_table "relationships", force: :cascade do |t|
